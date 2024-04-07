bbffcc6712507cbe7ed4a02977ae219ad34e3764
Jan Herbers <84963446+janherbers@users.noreply.github.com>
2024-04-07 22:00:07 +0200

    assignment 2

--- 4_Hardhat/hardhat.config.js
+++ 4_Hardhat/hardhat.config.js
@@ -1,6 +1,22 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env') });
 
 /** @type import('hardhat/config').HardhatUserConfig */
+//console.log("METAMASK_1_PRIVATE_KEY:", process.env.METAMASK_1_PRIVATE_KEY);
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.8.17",
+  defaultNetwork: "custom",
+  networks: {
+    custom: {
+      url: "http://134.155.50.136:8506",
+      chainId: 1337,
+      // accounts: [Buffer.from(process.env.METAMASK_1_PRIVATE_KEY, 'utf8').toString('hex')],
+      // accounts: [process.env.METAMASK_1_PRIVATE_KEY],
+      accounts: ["b48dd1e8092e96fa95c5bc08a9ff33998455292719dbc7defe20803209ce0668"]
+      //gas: "auto",
+      //gasPrice: "auto",
+      //gasMultiplier: 1
+    }
+  }
 };
